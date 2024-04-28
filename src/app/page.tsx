a7b46cb91e3f24d4299ac5ca46745506d0d4050f
Aniruddha Sil <welcometohellv@gmail.com>
2024-04-28 14:54:03 +0530

    Added metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 'use client'
 import usePongKaboom from '@/components/kaboom'
+import { Metadata } from 'next/types'
+
+export const metadata: Metadata = {
+  title: 'Pong Kaboom',
+  description: 'Pong game built with Kaboom.js',
+}
 
 export default function Home() {
   const { canvasRef } = usePongKaboom()
